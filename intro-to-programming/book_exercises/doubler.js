18bfc1be7b8003c0e240bd398f40f44d0d4ddf81
Ramses Oudt <96059866+logseqramses@users.noreply.github.com>
2023-07-15 18:49:27 +0200

    Added usage instructions to doubler.js

--- intro-to-programming/book_exercises/doubler.js
+++ intro-to-programming/book_exercises/doubler.js
@@ -1,6 +1,7 @@
 function doubler(number) {
   console.log(number);
 
+  // Set the debugger break point on the line below (line 5) and keep stepping into every step to see how the call stack develops.
   if (number <= 50) {
     doubler(number * 2);
   }
