c9d961e07f82f9537c1a97b19c22a947d0bb3ef1
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-16 20:23:15 +0200

    Added solution to 4: map-elements.js

--- intro-to-programming/book_exercises/arrays/map-elements.js
+++ intro-to-programming/book_exercises/arrays/map-elements.js
@@ -1,1 +1,15 @@
-// Solution to exercise 4 of the Arrays chapter
+/* Solution to exercise 4 of the Arrays chapter
+
+ Challenge:
+ - Use map() to create an array that contains one element for each element in the original array.
+- If the element is an even value, then the corresponding element in the new array should contain the string 'even'; otherwise, the element in the new array should contain 'odd'. */
+
+let myArray = [
+  1, 3, 6, 11,
+  4, 2, 4, 9,
+  17, 16, 0,
+];
+
+const myStringArray = myArray.map(num => num % 2 === 0 ? 'even' : 'odd');
+
+console.log(myStringArray);
