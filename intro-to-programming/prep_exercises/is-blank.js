8b49c9cfa8f24ba7765b62554c9b49e5685077d0
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-16 23:09:48 +0200

    Part 2 of Blank? 'Change your isBlank function from the previous exercise to return true if the string is empty or only contains whitespace'

--- intro-to-programming/prep_exercises/is-blank.js
+++ intro-to-programming/prep_exercises/is-blank.js
@@ -1,9 +1,9 @@
 /* Solution to: JS100 - JavaScript Basics/ Strings / Blank? Version 1 */
 
 function isBlank(str) {
-  return str.length === 0;
+  return str.trim().length === 0;
 }
 
 console.log(isBlank('mars')); // false
-console.log(isBlank('  '));   // false
+console.log(isBlank('  '));   // true
 console.log(isBlank(''));     // true
