b9f6bff9f13da880daf97f12a698c5b73d0a0572
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-17 19:18:02 +0200

    Solution to exercise 4: keys-to-uppercase.js

---
+++ intro-to-programming/book_exercises/objects/keys-to-uppercase.js
@@ -0,0 +1,13 @@
+/* Exercise 4: Create an array from the keys of the object obj below, with all of the keys converted to uppercase.
+
+Your implementation must not mutate obj. */
+
+let obj = {
+  b: 2,
+  a: 1,
+  c: 3,
+};
+
+const uppercaseKeys = Object.keys(obj).map(key => key.toUpperCase());
+
+console.log(uppercaseKeys);
