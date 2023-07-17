d45d18a47ea2ed0b428db37fc033f8f12be7e6f5
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-17 15:00:27 +0200

    Solution to exercise 1: first-element.js

---
+++ intro-to-programming/prep_exercises/arrays/first-element.js
@@ -0,0 +1,7 @@
+/* Exercise 1: Write a function that returns the first element of an input array.  */
+
+function first(arr) {
+  return(arr[0]);
+}
+
+console.log(first(['Earth', 'Moon', 'Mars'])); // 'Earth'
