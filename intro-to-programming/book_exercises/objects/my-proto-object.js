f4f7958fbb71dd578358f2c3755d85e9151e78ec
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-17 19:31:05 +0200

    Updated solution to exercise 5 with solution to exercise 7: my-proto-object.js

--- intro-to-programming/book_exercises/objects/my-proto-object.js
+++ intro-to-programming/book_exercises/objects/my-proto-object.js
@@ -6,6 +6,33 @@ let myProtoObj = {
 };
 
 const myObj = Object.create(myProtoObj);
+myObj.qux = 3;
 
-console.log(myObj.foo); // 1
-console.log(myObj.bar); // 2
+// console.log(myObj.foo); // 1
+// console.log(myObj.bar); // 2
+
+/* Exercise 7: Add a qux property with value 3 to the myObj object we created in the previous exercise. 
+
+Now, examine the following code snippets and determine whether these two snippets produce the same output? Why? */
+
+let objKeys = Object.keys(myObj);
+objKeys.forEach(function(key) {
+  console.log(key);
+});
+
+/* Produces:
+qux
+
+Why? Because Object.keys() only extracts the objects own properties.
+*/
+
+for (let key in myObj) {
+  console.log(key);
+}
+
+/* Produces:
+ qux
+ foo
+ bar
+
+ Why? Because a for...in loop first iterates over an objects own properties and then over the properties from the parent object. */
