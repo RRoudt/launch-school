6dd8d28cecc53cdda1de556c727138268cacb74f
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-17 15:21:41 +0200

    Solution to exercise 4: split-alphabet.js

---
+++ intro-to-programming/prep_exercises/arrays/split-alphabet.js
@@ -0,0 +1,8 @@
+/* Exercise 4: Split the string alphabet into an array of characters. */
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+const alphabetArray = alphabet.split('');
+
+console.log(alphabet);
+console.log(alphabetArray);
