f068ce0e5b81ea5ea52e130f70fb8205f5bae4df
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-18 19:04:56 +0200

    Solution to exercise 10: cloning-a-person.js

---
+++ intro-to-programming/prep_exercises/objects/cloning-a-person.js
@@ -0,0 +1,24 @@
+/* Exercise 10: Write a function clone that takes an object as argument and returns a shallow copy of that argument. */
+
+function clone(obj) {
+  return Object.assign({}, obj);
+}
+
+let person = {
+  title: 'Duke',
+  name: {
+    value: 'Nukem',
+    isStageName: true
+  },
+  age: 33
+};
+
+let clonedPerson = clone(person);
+person.age = 34;
+
+console.log(person.age);       // 34
+console.log(clonedPerson.age); // 33
+
+person.name.isStageName = false;
+console.log(person.name.isStageName);       // false
+console.log(clonedPerson.name.isStageName); // false
