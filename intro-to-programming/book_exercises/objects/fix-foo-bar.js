6aa7d0445c4dd2957d9e058c8ecacdc56f627a1a
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-18 18:21:48 +0200

    Solution to exercise 12: fix-foo-bar.js

---
+++ intro-to-programming/book_exercises/objects/fix-foo-bar.js
@@ -0,0 +1,10 @@
+/* Exercise 12: Without creating a new function or changing lines 5 or 6, update this code to work as intended. */
+
+function foo(bar) {
+  console.log(bar, bar, bar);
+}
+
+const bar = foo;
+
+foo("hello"); // should print "hello hello hello"
+bar("hi");    // should print "hi hi hi"
