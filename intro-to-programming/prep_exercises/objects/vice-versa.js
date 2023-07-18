ee6af833e4acb41f0d1f97b7295c15b9b742da1f
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-18 19:01:07 +0200

    Solution to exercise 9: vice-versa.js

---
+++ intro-to-programming/prep_exercises/objects/vice-versa.js
@@ -0,0 +1,13 @@
+/* Exercise 8: Convert nestedArray into an object, containing the same key-value pairs. */
+
+let nestedArray = [['title', 'Duke'], ['name', 'Nukem'], ['age', 33]];
+
+let person = {};
+
+nestedArray.forEach(element => {
+  person[element[0]] = element[1];
+});
+
+console.log(person);
+// Expected output:
+// { title: 'Duke', name: 'Nukem', age: 33 }
