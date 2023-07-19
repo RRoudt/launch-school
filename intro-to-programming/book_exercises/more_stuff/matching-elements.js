69f089bf34d340959c8dc0b6fc78df9297246ffa
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-19 21:59:03 +0200

    Solution to exercise 6: matching-elements.js

---
+++ intro-to-programming/book_exercises/more_stuff/matching-elements.js
@@ -0,0 +1,22 @@
+/* Exercise 6: Write a function that searches an array of strings for every element that matches the regular expression given by its argument. The function should return all matching elements in an array. */
+
+function allMatches(wordsArray, exp) {
+  let matchingWords = [];
+
+  for (let word of wordsArray) {
+    if (exp.test(word)) matchingWords.push(word);
+  }
+
+  return matchingWords;
+}
+
+let words = [
+  'laboratory',
+  'experiment',
+  'flab',
+  'Pans Labyrinth',
+  'elaborate',
+  'polar bear',
+];
+
+console.log(allMatches(words, /lab/)); // => ['laboratory', 'flab', 'elaborate']
