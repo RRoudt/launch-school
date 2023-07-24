5dff522c2d68a72735bb135456b285b9c2b8b8b9
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-07-24 22:44:54 +0200

    Working MVP (without prompting user for input) of mortgage calculator (assigment 19 of lesson 2)

---
+++ js101/lesson_2/mortgage-calculator.js
@@ -0,0 +1,89 @@
+/* Solution to assignment 19 of lesson 2: Mortgage Calculator
+
+Inputs:
+- Loan amount
+- Annual Percentage Rate (APR)
+- Loan duration in years
+
+Calculation:
+- Monthly interest rate
+- Loan duration in months
+
+Output:
+- Monthly payment
+
+Formula:
+let discountFactor = Math.pow((1 + monthlyInterestRate), -loanDurationInMonths);
+
+let monthlyPayment = loanAmount * (monthlyInterestRate / (1 - discountFactor));
+
+Pseudocode:
+GET loanAmount
+GET annualInterestRate
+GET loanDurationInYears
+
+SET loanDurationInMonths = loanDurationInYears / 12
+SET monthlyInterestRate = annualInterestRate / 12
+SET discountFactor = Math.pow((1 + monthlyInterestRate), -loanDurationInMonths)
+SET monthlyPayment = loanAmount * (monthlyInterestRate / (1 - discountFactor))
+
+PRINT loanDurationInMonths
+PRINT monthlyInterestRate
+PRINT monthlyPayment
+*/
+
+function getMonthlyPayment(loanAmount,
+  annualInterestRate, loanDurationInYears) {
+  let loanDurationInMonths = loanDurationInYears * 12;
+  let monthlyInterestRate = annualInterestRate / 100 / 12;
+  let monthlyInterestRatePercent = monthlyInterestRate * 100;
+
+  let discountFactor = Math.pow((1 + monthlyInterestRate),
+    -loanDurationInMonths);
+  let monthlyPayment = loanAmount * (monthlyInterestRate /
+    (1 - discountFactor));
+
+  console.log(`Loan duration: ${loanDurationInMonths} months`);
+  console.log(`Monthly interest rate: ${monthlyInterestRatePercent.toFixed(2)}%`);
+  console.log(`Monthly payment: $${monthlyPayment.toFixed(2)}`);
+}
+
+// Test Case 1:
+// Loan Amount: $150,000
+// Annual Interest Rate: 4.5%
+// Loan Duration: 30 years
+getMonthlyPayment(150000, 4.5, 30);
+// Expected Output:
+// Loan duration: 360 months
+// Monthly interest rate: 0.38%
+// Monthly payment: $760.03
+
+// Test Case 2:
+// Loan Amount: $350,000
+// Annual Interest Rate: 3.92%
+// Loan Duration: 20 years
+getMonthlyPayment(350000, 3.92, 20);
+// Expected Output:
+// Loan duration: 240 months
+// Monthly interest rate: 0.33%
+// Monthly payment: $2071.84
+
+// Test Case 3:
+// Loan Amount: $500,000
+// Annual Interest Rate: 2.75%
+// Loan Duration: 15 years
+getMonthlyPayment(500000, 2.75, 15);
+// Expected Output:
+// Loan duration: 180 months
+// Monthly interest rate: 0.23%
+// Monthly payment: $3381.29
+
+// Test Case 4:
+// Loan Amount: $250,000
+// Annual Interest Rate: 3.25%
+// Loan Duration: 10 years
+getMonthlyPayment(250000, 3.25, 10);
+// Expected Output:
+// Loan duration: 120 months
+// Monthly interest rate: 0.27%
+// Monthly payment: $2432.35
