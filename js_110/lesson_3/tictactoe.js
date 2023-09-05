923882cd7ea85323f8273050c71ca7ca1e6142ac
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2023-09-05 19:30:31 +0200

    Tic Tac Toe step 4: Determining the winner

--- js_110/lesson_3/tictactoe.js
+++ js_110/lesson_3/tictactoe.js
@@ -51,6 +51,7 @@ const COMPUTER_MARKER = 'O';
 
 function displayBoard(board) {
   console.clear();
+  console.log(`You are ${PLAYER_MARKER}. Computer is ${COMPUTER_MARKER}`);
   console.log('');
   console.log('     |     |     ');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}  `);
@@ -121,17 +122,65 @@ function boardFull(board) {
   return validChoices(board).length === 0;
 }
 
-function someoneWon(board) {
-  return false;
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],  // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],  // columns
+    [1, 5, 9], [3, 5, 7]              // diagonals
+  ];
+
+  for (let line = 0; line < winningLines.length; line++) {
+    let [ sq1, sq2, sq3 ] = winningLines[line];
+
+    if (
+      board[sq1] === PLAYER_MARKER &&
+      board[sq2] === PLAYER_MARKER &&
+      board[sq3] === PLAYER_MARKER
+    ) {
+      return 'Player';
+
+    } else if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
 }
 
-let board = initializeBoard();
-displayBoard(board);
+function someoneWon(board) {
+  return !!detectWinner(board);
+}
 
+// Main game loop
 while (true) {
-  playerChoosesSquare(board);
-  computerChoosesSquare(board);
+  let board = initializeBoard();
+
+  while (true) {
+    displayBoard(board);
+
+    playerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+
+    computerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+  }
+
   displayBoard(board);
 
-  if (someoneWon(board) || boardFull(board)) break;
+  if (someoneWon(board)) {
+    prompt(`${detectWinner(board)} won!`);
+  } else {
+    prompt("It's a tie!");
+  }
+
+  prompt('Play again? (y/n)');
+  let playAgain = readline.question().toLowerCase()[0];
+  if (playAgain !== 'y') break;
 }
+
+console.clear();
+prompt('Thanks for playing Tic Tac Toe!');
