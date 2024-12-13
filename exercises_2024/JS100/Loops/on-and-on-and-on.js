39a89ed85799c17194567458ee792542d35c014d
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-13 19:16:18 +0100

    On and on and on exercise

---
+++ exercises_2024/JS100/Loops/on-and-on-and-on.js
@@ -0,0 +1,8 @@
+// The following code keeps looping forever. (You can hit Ctrl-C to stop it.) Why is that? Also modify it so that it stops after the first iteration.
+
+//It keeps running forever because the conditional is missing
+
+for (let i = 0; ; i += 1) {
+  console.log("and on");
+  break;
+}
