202854a602864d4c7e2e2e2b18bf80ca5e94066b
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-15 21:43:00 +0100

    JS100 exercise: Truthy vs Falsy

---
+++ exercises_2024/JS100/Conditionals/truthy-vs-falsy.js
@@ -0,0 +1,16 @@
+// Without looking at your notes or MDN, try to recall all values that count as falsy in JavaScript.
+
+// 0
+console.log(0 == false);
+
+// ''
+console.log('' == false);
+
+// undefined
+console.log(undefined == false);
+
+// null
+console.log(null == false);
+
+// false
+console.log(false == false);
