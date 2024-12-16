69e1133ca6969467e61c84d3ed872c32222f7b89
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:32:46 +0100

    JS100 Functions 1.4: Squared Number

---
+++ exercises_2024/JS100/Functions_1/squared-number.js
@@ -0,0 +1,14 @@
+/*
+Write a function that accepts a single argument, a number, and returns the result of multiplying its argument by an exponent of 2 (also known as squaring the number).
+
+Rules:
+- Write a function
+- Accept a single argument (number)
+- Return the passed-in number, squared (multiplied it by an exponent of 2)
+*/
+
+function squaredNumber(number) {
+  return number ** 2;
+}
+
+console.log(squaredNumber(3)); // 9
