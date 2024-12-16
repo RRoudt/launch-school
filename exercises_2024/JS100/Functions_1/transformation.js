575be6a7406961f04f93b6f1c726b5dbab465c8e
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:42:44 +0100

    JS100 Functions 1.7: Transformation

---
+++ exercises_2024/JS100/Functions_1/transformation.js
@@ -0,0 +1,8 @@
+/*
+Use JavaScript's string methods on the string 'Captain Ruby' to produce the string 'Captain JavaScript'.
+*/
+
+let string = 'Captain Ruby';
+let transformedString = string.replace('Ruby', 'JavaScript');
+
+console.log(transformedString);
