d7841b55e8adf8aad2b99251955334720f46d349
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:22:37 +0100

    JS100 Functions 1.1: Sum

---
+++ exercises_2024/JS100/Functions_1/sum.js
@@ -0,0 +1,12 @@
+/* Examine the example function invocation below. Write the function sum, such that it accepts two arguments and returns the sum of its arguments. You may assume that the function arguments will always be numbers.
+
+Rules:
++ Accept two arguments (numbers)
++ Return the sum of the two arguments (numbers)
+*/
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(22, 10)); // 32
