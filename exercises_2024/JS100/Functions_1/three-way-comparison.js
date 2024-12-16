a64805f442cadacd5e6b4737d7c0f2d2edda5ebb
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:40:45 +0100

    JS100 Functions 1.6: Three-way comparison

--- exercises_2024/JS100/Functions_1/three-way-comparison.js
+++ exercises_2024/JS100/Functions_1/three-way-comparison.js
@@ -2,12 +2,12 @@
 Write a function that compares the length of two strings. It should return -1 if the first string is shorter, 1 if the second string is shorter, and 0 if they're of equal length, as in the following example:
 
 Rules:
-- Write a function
-- Accept two string arguments
-- Compare the length of the arguments
-  - If the first string is shorter, return -1
-  - If the second string is shorter, return 1
-  - If both strongs are of equal length, return 0
++ Write a function
++ Accept two string arguments
++ Compare the length of the arguments
+  + If the first string is shorter, return -1
+  + If the second string is shorter, return 1
+  + If both strongs are of equal length, return 0
 */
 
 function compareByLength(stringA, stringB) {
