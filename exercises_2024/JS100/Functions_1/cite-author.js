f8d8143b9dae7d0a2ca60c316f0682048adacc0f
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:30:07 +0100

    JS100 Functions 1.3: Cite the Author

---
+++ exercises_2024/JS100/Functions_1/cite-author.js
@@ -0,0 +1,17 @@
+/*
+Implement a function cite that takes two string arguments: the author of the quote and what they said. It then logs the quote to the console, as in the following example.
+
+Rules:
++ Accept two string arguments:
+  + Author
+  + Quote
++ Log the author and quote to the console
+*/
+
+function cite(author, quote) {
+  console.log(`${author} said: "${quote}"`);
+}
+
+cite('Brendan Eich', 'Always bet on JavaScript.');
+// logs:
+// Brendan Eich said: "Always bet on JavaScript."
