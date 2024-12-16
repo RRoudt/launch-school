58b4a3327f23304772de6380b6d410bd752e1adc
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:52:40 +0100

    JS100 Functions 1.9: Locale, Part 1

--- exercises_2024/JS100/Functions_1/locale-pt-1.js
+++ exercises_2024/JS100/Functions_1/locale-pt-1.js
@@ -1,3 +1,18 @@
 /*
+Write a function that extracts the language code from a locale. A locale is a combination of a language code, a region, and usually also a character set, e.g en_US.UTF-8.
 
+Rules:
++ Write a function
++ Extract the language code from a locale. Example:
+  + Locale: 'en_US.UTF-8'
+  + Language code: 'en'
 */
+
+function extractLanguage(locale) {
+  return locale.slice(0, 2);
+}
+
+// Test cases
+console.log(extractLanguage('en_US.UTF-8'));  // 'en'
+console.log(extractLanguage('en_GB.UTF-8'));  // 'en'
+console.log(extractLanguage('ko_KR.UTF-16')); // 'ko'
