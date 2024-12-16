903ed1d313a469503918fd294e768cf550881ed3
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-16 22:24:59 +0100

    JS100 Functions 1.2: Log Quote

---
+++ exercises_2024/JS100/Functions_1/log-quote.js
@@ -0,0 +1,16 @@
+/*
+Write a function that logs Brendan Eich's quote 'Always bet on JavaScript.' to the console. What is the return value of the function?
+
+Rules:
++ Take no arguments
++ Log string to the console: 'Always bet on JavaScript.'
++ Return undefined
+*/
+
+function brendanEichQuote() {
+  console.log('Always bet on JavaScript.');
+}
+
+brendanEichQuote();
+// logs:
+// Always bet on JavaScript.
