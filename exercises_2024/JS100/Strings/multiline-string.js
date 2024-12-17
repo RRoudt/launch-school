54833f2c5aeff5f463e6ed382601fd59d5d087a0
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-17 19:45:25 +0100

    JS100 Strings 4: Multiline String

---
+++ exercises_2024/JS100/Strings/multiline-string.js
@@ -0,0 +1,19 @@
+/*
+Take the following rhyme:
+
+  A pirate I was meant to be!
+  Trim the sails and roam the sea!
+
+How can you assign this string to a single variable, preserving the line break?
+
+Problem:
+- Store a string with a line break, in a single variable
+
+Question:
+- What is the character for a line break in JS?
+  - Answer: \n
+*/
+
+let string = "A pirate I was meant to be!\nTrim the sails and roam the sea!";
+
+console.log(string);
