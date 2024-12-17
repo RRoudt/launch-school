c8472e651294fc62e776d0d0318ec852f6706286
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-17 19:37:19 +0100

    JS100 Strings 2: ALL CAPS

---
+++ exercises_2024/JS100/Strings/all-caps.js
@@ -0,0 +1,6 @@
+// Take the string 'confetti floating everywhere' and transform it to upper case.
+
+let string = 'confetti floating everywhere';
+let upperString = string.toUpperCase();
+
+console.log(upperString);
