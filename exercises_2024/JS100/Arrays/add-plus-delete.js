587f7223f48856d15d34716273491f80c37ca168
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-18 17:49:06 +0100

    JS100 - Arrays - 3. Add + Delete

---
+++ exercises_2024/JS100/Arrays/add-plus-delete.js
@@ -0,0 +1,27 @@
+/* JS100 - Arrays - 3. Add + Delete
+
+Instructions:
+- We are given the following array of energy sources:
+  ['fossil', 'solar', 'wind', 'tidal', 'fusion']
+
+  Remove 'fossil' from the array, then add 'geothermal' to the end of the array.
+
+Input:
+- An array
+
+Output:
+- Same array, but changed:
+  - 'fossil' removed
+  'geothermal' added
+
+Data:
+- Array
+*/
+
+let energy = ['fossil', 'solar', 'wind', 'tidal', 'fusion'];
+
+energy.shift();
+energy.unshift('geothermal');
+
+// Test case
+console.log(energy); // ['geothermal', 'solar', 'wind', 'tidal', 'fusion'];
