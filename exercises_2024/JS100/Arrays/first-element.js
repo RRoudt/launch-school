9c90cdc1809fce623ef1ea9cbf179811ed3362f1
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-18 17:40:14 +0100

    JS100 - Arrays - 1. First Element

---
+++ exercises_2024/JS100/Arrays/first-element.js
@@ -0,0 +1,21 @@
+/* JS100 - Arrays - 1. First Element
+
+Instructions:
++ Write a function that returns the first element of an input array. 
+
+Input:
++ Array
+
+Output:
++ First element if input array
+
+Data:
++ Array
+*/
+
+function first(arr) {
+  return arr[0];
+}
+
+// Test case
+console.log(first(['Earth', 'Moon', 'Mars'])); // 'Earth'
