43d4057ed3f5db5cfa22c72263ccae3ed6933b77
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2024-12-27 21:51:47 +0100

    Easy 2.3: Multiplying Two Numbers

---
+++ exercises_2024/easy_2/multplying-two-numbers.js
@@ -0,0 +1,13 @@
+// Easy 2.3: Multiplying Two Numbers
+// Create a function that takes two arguments, multiplies them together, and returns the result.
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Test cases
+console.log(multiply(5, 3) === 15);   // logs true
+console.log(multiply(5, 4) === 20);   // logs true
+console.log(multiply(7, 3) === 21);   // logs true
+console.log(multiply(50, 3) === 150); // logs true
+console.log(multiply(5, 30) === 150); // logs true
