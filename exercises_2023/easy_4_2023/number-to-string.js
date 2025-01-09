40c26610cb2cd4ce27f3debde52b9feef065ca4d
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2025-01-09 22:26:24 +0100

    Stragglers

--- exercises_2023/easy_4_2023/number-to-string.js
+++ exercises_2023/easy_4_2023/number-to-string.js
@@ -1,11 +1,33 @@
-function doubleNumbers(numbers) {
-  for (let num of numbers) {
-    num *= 2;
+// Create an object that expresses the frequency with which each letter occurs in this string:
+// { T: 1, h: 1, e: 2, F: 1, l: 1, ... }
+// Algorithm:
+// Create an empty object to keep track of the letters: letterObj
+// Iterate over the string
+//   If the current character is a property in letterObj:
+//     Increase property-value by 1
+//   Else:
+//     Add property to letterObj and set the value to 1
+//   Endif
+// End iterating over string
+// Return letterObj
+//
+// Q: Ignore spaces?
+//   A: Let's assume, yes
+
+function countLetters(string) {
+  let letterObj = {};
+  
+  for (let char of string) {
+    if (char === ' ') continue;
+    
+    letterObj[char] = letterObj.hasOwnProperty(char) ?
+      letterObj[char] += 1 :
+      letterObj[char] = 1;
   }
   
-  return numbers;
+  return letterObj;
 }
 
-let myNumbers = [1, 4, 3, 7, 2, 6];
-doubleNumbers(myNumbers); // => [2, 8, 6, 14, 4, 12]
-myNumbers;                // => [1, 4, 3, 7, 2, 6]
+let statement = "The Flintstones Rock";
+
+console.log(countLetters(statement));
