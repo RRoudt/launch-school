27fe68efc86941c95b60347ec0c38e1ecfdb6585
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2026-04-30 15:21:08 +0200

    Update calculator.py
    11. Walk-through: Refactoring Calculator

--- python-track/py101/lesson_2/calculator.py
+++ python-track/py101/lesson_2/calculator.py
@@ -4,48 +4,52 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f"==> {message}")
+
+prompt('Welcome to Calculator!')
 
 # Ask the user for the first number
 while True:
-    number1 = input("What's the first number?\n")
+    prompt("What's the first number?")
+    number1 = input()
     try:
         number1 = int(number1)
         break
     except:
-        print("That's not a valid number")
+        prompt("That's not a valid number")
         continue
 
 # Ask the user for the second number
 while True:
-    number2 = input("What's the second number?\n")
+    prompt("What's the second number?")
+    number2 = input()
     try:
         number2 = int(number2)
         break
     except:
-        print("That's not a valid number")
+        prompt("That's not a valid number")
         continue
 
 # Ask the user for the operation to perform
 operation = ''
 while operation not in ('1', '2', '3', '4'):
-    operation = input('What operation would you like to perform?\n'
-                  '1) Add 2) Subtract 3) Multiply 4) Divide\n')
+    prompt("""What operation would you like to perform?
+    1) Add 2) Subtract 3) Multiply 4) Divide""")
+    operation = input()
 
 # Perform and print the operation on the two numbers
 match operation:
     case '1':
         output = number1 + number2
-        print(f"{output}")
     case '2':
         output = number1 - number2
-        print(f"{output}")
     case '3':
         output = number1 * number2
-        print(f"{output}")
     case '4':
         try:
             output = number1 / number2
-            print(f"{output}")
         except:
-            print("You can't divide by 0. Please choose another second number.")
+            prompt("You can't divide by 0. Please choose another second number.")
+
+prompt(f"The result is: {output}")
