4504a27cda11b8591bcf5ec39706ec96c08ae99c
Ramses Oudt <3370414+RRoudt@users.noreply.github.com>
2026-05-04 21:14:00 +0200

    Create easy_1_5.py

---
+++ python-track/py101/lesson_3/easy_1_5.py
@@ -0,0 +1,5 @@
+# Print the string with the case of all letters swapped:
+
+munsters_description = "The Munsters are creepy and spooky."
+print(munsters_description.swapcase())
+# "tHE mUNSTERS ARE CREEPY AND SPOOKY."
